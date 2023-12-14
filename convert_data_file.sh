eabb926dbb3053cca6bda76f8cafcdd3996bfd57
thinhvd <20021444@vnu.edu.vn>
2023-12-15 00:40:09 +0700

    update prepare data scripts

--- convert_data_file.sh
+++ convert_data_file.sh
@@ -4,14 +4,39 @@ data_dir="./data"
 
 mkdir -p "${data_dir}/train_clean_data"
 mkdir -p "${data_dir}/test_clean_data"
+mkdir -p "${data_dir}/train_other_data"
+mkdir -p "${data_dir}/test_other_data"
 
-# Extract train data
-echo "Extracting train data..."
-tar -xzf "${data_dir}/dev-clean.tar.gz" -C "${data_dir}/train_clean_data"
+# Function to extract data
+extract_data() {
+  archive=$1
+  destination=$2
+  echo "Extracting data from $archive to $destination..."
+  if tar -xzf "$archive" -C "$destination" &; then
+    echo "Extraction started successfully."
+  else
+    echo "Extraction failed. Exiting..."
+    exit 1
+  fi
+}
 
-# Extract test data
-echo "Extracting test data..."
-tar -xzf "${data_dir}/test-clean.tar.gz" -C "${data_dir}/test_clean_data"
+# Extract train clean data
+extract_data "${data_dir}/dev-clean.tar.gz" "${data_dir}/train_clean_data"
+
+# Extract test clean data
+extract_data "${data_dir}/test-clean.tar.gz" "${data_dir}/test_clean_data"
+
+# Extract train other data
+extract_data "${data_dir}/dev-other.tar.gz" "${data_dir}/train_other_data"
+
+# Extract test other data
+extract_data "${data_dir}/test-other.tar.gz" "${data_dir}/test_other_data"
+
+# Wait for all background processes to finish
+wait
+
+echo "All extractions completed successfully."
+
 
 #-----------------------------------------------------------------#
 
--- download_data.sh
+++ download_data.sh
@@ -10,8 +10,11 @@
 #   exit 1
 # fi
 
-train_data_url="https://www.openslr.org/resources/12/dev-clean.tar.gz"
-test_data_url="https://www.openslr.org/resources/12/test-clean.tar.gz"
+train_clean_data_url="https://www.openslr.org/resources/12/dev-clean.tar.gz"
+test_clean_data_url="https://www.openslr.org/resources/12/test-clean.tar.gz"
+train_other_data_url="https://www.openslr.org/resources/12/dev-other.tar.gz"
+test_other_data_url="https://www.openslr.org/resources/12/test-other.tar.gz"
+
 data_dir="./data"
 
 # Function to check if URL is valid
@@ -28,9 +31,9 @@ check_url() {
 # Function to download data
 download_data() {
   url=$1
-  echo "Downloading data from $url..."
-  if wget -O "${data_dir}/$(basename $url)" "$url"; then
-    echo "Download completed successfully."
+  echo "Downloading data from $url to $data_dir/..."
+  if wget -q --show-progress -O "${data_dir}/$(basename $url)" "$url" &; then
+    echo "Download started successfully."
   else
     echo "Download failed. Exiting..."
     exit 1
@@ -38,14 +41,21 @@ download_data() {
 }
 
 # Check URLs
-check_url "$train_data_url"
-check_url "$test_data_url"
+check_url "$train_clean_data_url"
+check_url "$test_clean_data_url"
+check_url "$train_other_data_url"
+check_url "$test_other_data_url"
 
 # Create data directory if it doesn't exist
 mkdir -p "$data_dir"
 
-# Download train data
-download_data "$train_data_url"
+# Download data
+download_data "$train_clean_data_url"
+download_data "$test_clean_data_url"
+download_data "$train_other_data_url"
+download_data "$test_other_data_url"
 
-# Download test data
-download_data "$test_data_url"
+# Wait for all background processes to finish
+wait
+
+echo "All downloads completed successfully."
